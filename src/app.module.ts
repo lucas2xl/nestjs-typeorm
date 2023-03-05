e2ad5e7d7f030e66c957948ff16fb3f249c8bb37
Lucas Aguiar <luqkinhaas@gmail.cim>
2023-03-05 17:27:28 -0300

    chore: add .env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoursesModule } from './courses/courses.module';
-import { databaseOptions } from './database/database';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseOptions), CoursesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CoursesModule,
+  ],
   controllers: [],
   providers: [],
 })
